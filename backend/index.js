578a3167559420e57d9e7e11c71a0949885b3d95
Franck Asket <32412954+asketsystem@users.noreply.github.com>
2022-10-07 18:43:03 +0000

    Network to mysql database

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,29 @@
 import express from "express";
+import mysql from "mysql";
 
 const app = express();
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "sys",
+});
+
+// If there is a auth problem
+// ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root'
+
+app.get("/", (req, res) => {
+  res.json("hello this is the backend");
+});
+
+app.get("/books", (req, res) => {
+  const q = "SELECT * FROM books";
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
 
 app.listen(8800, () => {
   console.log("Connected to backend");
