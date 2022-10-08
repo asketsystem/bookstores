5fc1750d51c2b7e8b0f9c439a9ae62f38cb9a615
Franck Asket <32412954+asketsystem@users.noreply.github.com>
2022-10-08 14:48:35 +0000

    fetch data from backend && mysql to client

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import mysql2 from "mysql2";
+import cors from "cors";
 
 const app = express();
 
@@ -14,6 +15,7 @@ const db = mysql2.createConnection({
 // ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root'
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.json("hello this is the backend");
@@ -28,8 +30,13 @@ app.get("/books", (req, res) => {
 });
 
 app.post("/books", (req, res) => {
-  const q = "INSERT INTO books (`title`, `desc`, `cover`) VALUES (?)";
-  const values = [req.body.title, req.body.desc, req.body.cover];
+  const q = "INSERT INTO books (`title`, `desc`,`price`, `cover`) VALUES (?)";
+  const values = [
+    req.body.title,
+    req.body.desc,
+    req.body.price,
+    req.body.cover,
+  ];
 
   db.query(q, [values], (err, data) => {
     if (err) return res.json(err);
